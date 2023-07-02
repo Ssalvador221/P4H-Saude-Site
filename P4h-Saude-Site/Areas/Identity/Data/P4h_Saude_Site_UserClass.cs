6fd46074a59ceb9d1adc4a5ab2f15a73deaecfa3
Ssalvador221 <j8731615@gmail.com>
2023-07-02 18:13:19 -0300

    First commit

---
+++ P4h-Saude-Site/Areas/Identity/Data/P4h_Saude_Site_UserClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace P4h_Saude_Site.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the P4h_Saude_Site_UserClass class
+public class P4h_Saude_Site_UserClass : IdentityUser
+{
+}
+
---
+++ P4h-Saude-Site/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using P4h_Saude_Site.Areas.Identity.Data;
+using P4h_Saude_Site.Data;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = "server=localhost;user=LAPTOP-09SI7DE3;password=123456;database=p4h-user-data";
+
+builder.Services.AddDbContext<P4h_Saude_SiteContext>(options => options.UseMySql(connectionString, new MySqlServerVersion(new Version(8,0,33))));
+
+builder.Services.AddDefaultIdentity<P4h_Saude_Site_UserClass>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<P4h_Saude_SiteContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
