d05d6e8acb651a7b295945f37659ef55380643d0
Ssalvador221 <j8731615@gmail.com>
2023-07-03 14:00:01 -0300

    Finish Site

---
+++ P4h-Saude-Site/Controllers/Paginas.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P4h_Saude_Site.Controllers
+{
+    public class Paginas : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AppApoio()
+        {
+            return View("AppApoio", "Paginas");
+        }
+
+        public IActionResult VendaOnline()
+        {
+            return View("VendaOnline", "Paginas");
+        }
+
+        public IActionResult TabelaDeVendas()
+        {
+            return View("TabelaDeVendas", "Paginas");
+        }
+
+        public IActionResult Documentos()
+        {
+            return View("Documentos", "Paginas");
+        }
+
+        public IActionResult Coparticipacao()
+        {
+            return View("Coparticipacao", "Paginas");
+        }
+
+    }
+}
